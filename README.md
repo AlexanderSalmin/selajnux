51befdd4e0863ebfa7c5ecdb4e9941f097744f3f
Alexander Salmin <alexander@salmin.biz>
2016-10-20 04:28:16 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # selajnux
-Handle selinux in Hiera
+
+#### Table of Contents
+
+1. [Overview](#overview)
+2. [Usage](#usage) 
+3. [Reference](#reference)
+
+## Overview
+
+Enable selinux booleans with hiera based on audit2allow.
+
+1) tail -2 /var/log/audit/audit.log | audit2allow
+2) Look at the output.
+3) selajnux::allow:
+     - httpd_can_network_relay
+     - httpd_can_network_connect
+
+## Usage
+
+Puppetfile
+
+  mod 'alexandersalmin/selajnux', :latest
+
+Puppetnode;
+
+  include selajnux
+
+Hiera;
+
+  selajnux::allow:
+   - httpd_can_network_connect
+
+  selajnux::deny:
+   - httpd_can_network_relay
+
+
+## Reference
+
+https://docs.puppet.com/puppet/latest/reference/type.html#selboolean-attributes
---
+++ manifests/init.pp
@@ -0,0 +1,55 @@
+# Class: selajnux
+# ===========================
+#
+# Handle selinux in hiera.
+#
+# Parameters
+# ----------
+#
+# * `selajnux::allow`
+# Configure selinux boolean value to "on".
+# 
+# * `selajnux::deny`
+# Configure selinux boolean value to "off".
+#
+# Variables
+# ----------
+#
+# No required variables.
+#
+# Examples
+# --------
+#
+# @example
+#    include selajnux
+#    
+#    selajnux::allow:
+#      - httpd_can_network_connect
+#      - httpd_can_network_relay
+#
+# Authors
+# -------
+#
+# Alexander Salmin
+#
+class selajnux ( $allow = [], $deny = [],) {
+
+    # Allow the following selinux booleans
+    # In hiera: selajnux::allow
+    $allow.each | $name | {
+        selboolean { "$name":
+          name => "$name",
+          value => "on",
+        }
+    }
+
+    # Deny the following selinux booleans
+    # In hiera: selajnux::deny
+    $deny.each | $name | {
+        selboolean { "$name":
+          name => "$name",
+          value => "off",
+        }
+    }
+
+} 
